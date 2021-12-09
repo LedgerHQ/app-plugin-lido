8d71e0ab40557a99262110ca5b1e34c088afb477
Alexandre Paillier <alexandre.paillier@ledger.fr>
2021-12-09 10:31:42 +0100

    Update to latest plugin sdk

--- src/handle_provide_token.c
+++ src/handle_provide_token.c
@@ -1,8 +1,8 @@
 #include "lido_plugin.h"
 
 void handle_provide_token(void *parameters) {
-    ethPluginProvideToken_t *msg = (ethPluginProvideToken_t *) parameters;
-    PRINTF("plugin provide token: 0x%p, 0x%p\n", msg->token1, msg->token2);
+    ethPluginProvideInfo_t *msg = (ethPluginProvideInfo_t *) parameters;
+    PRINTF("plugin provide token: 0x%p, 0x%p\n", msg->item1, msg->item2);
 
     msg->result = ETH_PLUGIN_RESULT_OK;
 }
